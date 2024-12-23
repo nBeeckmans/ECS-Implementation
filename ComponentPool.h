7cce1c039fa58058901bba5c5e2d07c094c38c34
nbeeckmans <nicolas.beeckmans@hotmail.com>
2024-12-23 16:24:20 -0500

    Add MAX_ENTITIES constant to for componentPool

--- ComponentPool.h
+++ ComponentPool.h
@@ -1,15 +1,16 @@
 #pragma once
 #include <cstddef>
 
+constexpr int MAX_ENTITIES = 32;
 typedef char byte;
 
 class ComponentPool {
 private:
-    byte* pPool = nullptr;
-    std::size_t elementSize = 0;
+    byte* pPool {nullptr};
+    std::size_t elementSize {0};
 public:
     explicit ComponentPool(std::size_t elementSize) : elementSize(elementSize) {
-        pPool = new byte[elementSize];
+        pPool = new byte[elementSize * MAX_ENTITIES];
     };
 
     ~ComponentPool() {
